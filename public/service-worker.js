c481855e7478bc029db9931a4638bbdd54e10f27
Seth Purr <eric.purrington@gmail.com>
2020-07-08 11:59:15 -0700

    service-worker written

---
+++ public/service-worker.js
@@ -0,0 +1,62 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/db.js",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
+var CACHE = "budget-cache-v1";
+const DATA_CACHE = "data-cache-v1";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+      caches.open(CACHE).then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    const currentCaches = [CACHE, DATA_CACHE];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+      }).then(cachesToDelete => {
+        return Promise.all(cachesToDelete.map(cacheToDelete => {
+          return caches.delete(cacheToDelete);
+        }));
+      }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE).then(cache => {
+                return fetch(event.request).then(response => {
+                    if (response.status == 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                }).catch(err => {
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+        )
+        return;
+    }
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request).then(response => {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/");
+                }
+            });
+        })
+    );
+});
